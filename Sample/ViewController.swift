3b06202c8a9272857d2c2c1ab892b19d28310c9e
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2017-11-30 18:54:00 -0200

    first commit

--- Sample/ViewController.swift
+++ Sample/ViewController.swift
@@ -8,11 +8,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate ,UITextViewDelegate {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func background(_ sender: Any) {
+        if self.view.backgroundColor === UIColor.white {
+            self.view.backgroundColor = UIColor.purple
+            label.textColor = UIColor.white
+        } else{
+            self.view.backgroundColor = UIColor.white
+            label.textColor = UIColor.black
+        }
+    }
+    @IBAction func tapped(_ sender: Any) {
+        if label.text == "😀"{
+            label.text = "🤯"
+        } else{
+            label.text = "😀"
+        }
+
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        if textView.text.count > 20 {
+            alert(message: "Campo com o máximo de 20 caracteres")
+        }
+    }
+    
+   
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if (textField.text!.count > 4){
+            alert(message: "Campo com o máximo de 4 caracteres")
+        }
+    }
+
+    
+    @IBAction func verifyText(_ sender: Any) {
+        
+        
+        if textView.text.count > 20 {
+            alert(message: "Campo com o máximo de 20 caracteres")
+        }
+        
+    }
+    
+    func alert(message: String, title: String = "") {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func addHello(to: String) -> String{
+        
+        let hello = "Hello"
+        
+        return to + hello
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        textView.delegate = self
+        //Without setting the delegate you won't be able to track UITextView events
+        //Sem adicionar o delegate, não conseguimos rastrear os eventos de UITextView
+        
+        textField.delegate = self
+        
+        _ = addHello(to: "World")
     }
 
     override func didReceiveMemoryWarning() {
